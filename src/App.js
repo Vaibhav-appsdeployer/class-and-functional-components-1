fd48c933e86b048a0d8c7b22cfc22d80ad7cf547
vaibhav <vaibhavkharat.ex@gmail.com>
2023-02-08 18:07:45 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from './components/Navbar'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar  />
+      <h1>Heading</h1>
+      <p>Paragraph</p>
     </div>
   );
 }
 
 export default App;
+
+
+// Most of the time we use function components instent of class components Bcoz it is easy to implement and better to understand  
+
+// 1. import react and another files that you want to add in this file 
+// 2.  create a function name it based on your file name (else to mantain)
+// 3. in this function return the main structure (main code that contains the basic structure) 
+// 4. at the end Export the default function
+
+
+
+
+
+
+// import React, { Component } from "react";
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>this is also a heading</h1>
+//         <p> This is para</p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+
+
+
